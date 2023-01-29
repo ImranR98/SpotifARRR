9bb9e85a1d89c044176c7fff27b4bd80dde97d76
Imran Remtulla <imranr98@pm.me>
2023-01-29 06:16:28 -0500

    Progress

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const env = require('./lib/env')
 const api = require('./lib/api')
+const open = require('open')
 
-const PORT = 8080
-
+const PORT = 6660
 const clientCreds = env.getClientCreds()
 
-const apiRequest = (path, method, authCode, body) => api.apiRequest(path, method, authCode, body, PORT, clientCreds.id, clientCreds.secret)
-
 const main = async () => {
-    console.log('Log in at the the following URL and allow SpotifARRR to access your playlists:\n' +
-        api.generateAuthURL(clientCreds.id, PORT))
-    authCode = await api.waitForAuthCode(PORT)
-    const myPlaylists = await apiRequest('/me/playlists', 'GET', authCode)
-    console.log(myPlaylists)
+    // Creds and helper funcs
+    // const clientCreds = {}
+    // if (process.argv.length != 4) {
+    //     throw 'Provide client ID and secret as arguments'
+    // } else {
+    //     clientCreds.id = process.argv[2]
+    //     clientCreds.secret = process.argv[3]
+    // }
+    open(api.generateAuthURL(clientCreds.id, PORT))
+    const authCode = await api.waitForAuthCode(PORT)
+    const getAccessToken = async () => await api.getAccessToken(authCode, PORT, clientCreds.id, clientCreds.secret)
+    var accessToken = await getAccessToken()
+    const requestArray = async (path) => {
+        if ((new Date()).valueOf() > accessToken.expiry.valueOf()) {
+            accessToken = await getAccessToken()
+        }
+        return await api.apiRequestGetArray(path, accessToken.access_token)
+    }
+    // Get data
+    const myPlaylists = await requestArray('/me/playlists')
+    for (var i = 0; i < myPlaylists.length; i++) {
+        myPlaylists[i].tracks = await requestArray(`/playlists/${myPlaylists[i].id}/tracks`)
+    }
+    console.log(JSON.stringify(myPlaylists, null, '\t'))
 }
 
 main().catch((e) => {
